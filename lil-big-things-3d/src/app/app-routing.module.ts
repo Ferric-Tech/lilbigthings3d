3b6b8636ca604d7c446a49b03ddaa109b607a4aa
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-02-04 14:37:13 +0200

    Basic routing implemented

--- lil-big-things-3d/src/app/app-routing.module.ts
+++ lil-big-things-3d/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPageComponent } from './pages/admin-page/admin.page';
+import { HomePageComponent } from './pages/home-page/home.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'admin', component: AdminPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
