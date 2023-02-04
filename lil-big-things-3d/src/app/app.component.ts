cc9b4f84e3f4cd6c34eb9178d01468d5d17a1da6
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-02-04 11:22:41 +0200

    Init ESLint and Firebase

--- lil-big-things-3d/src/app/app.component.ts
+++ lil-big-things-3d/src/app/app.component.ts
@@ -1,10 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Component } from '@angular/core';
+import { initializeApp } from '@angular/fire/app';
+import { createUserWithEmailAndPassword, getAuth } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'lil-big-things-3d';
+  app = initializeApp(environment.firebase);
+  auth = getAuth();
+
+  signUp(email: string, password: string) {
+    createUserWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
 }
