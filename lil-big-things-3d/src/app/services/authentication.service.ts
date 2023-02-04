b3f0c7d3f3a0753de234759bbbe7c4a2ce68b137
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-02-04 15:02:22 +0200

    signInWithEmail method added to auth service

--- lil-big-things-3d/src/app/services/authentication.service.ts
+++ lil-big-things-3d/src/app/services/authentication.service.ts
@@ -1,41 +1,56 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@angular/core';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from '@angular/fire/auth';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from '@angular/fire/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
-    auth = getAuth();
+  auth = getAuth();
 
-    get userID() {
-        onAuthStateChanged(this.auth, (user) => {
-            if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            return user.uid;
-            } 
-            return null
-        });
-        // User is signed out
+  get userID(): string | null {
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        return user.uid;
+      }
+      return null;
+    });
+    // User is signed out
+    // ...
+    return null;
+  }
+
+  signUpWithEmail(email: string, password: string): void {
+    createUserWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
         // ...
-        return null
-    } 
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
 
-    signUpWithEmail(email: string, password: string) {
-        createUserWithEmailAndPassword(this.auth, email, password)
-            .then((userCredential) => {
-            // Signed in
-            const user = userCredential.user;
-            // ...
-            })
-            .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-            });
-    }
-
-
-
+  signInWithEmail(email: string, password: string): void {
+    signInWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  }
 }
