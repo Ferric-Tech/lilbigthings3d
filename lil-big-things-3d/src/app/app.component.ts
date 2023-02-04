c117c26c6080be4fbea6aed73a42a4cd91dd4885
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-02-04 14:30:38 +0200

    Moving signUpWithEmail method to auth service

--- lil-big-things-3d/src/app/app.component.ts
+++ lil-big-things-3d/src/app/app.component.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Component } from '@angular/core';
 import { initializeApp } from '@angular/fire/app';
-import { createUserWithEmailAndPassword, getAuth } from '@angular/fire/auth';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -12,19 +11,4 @@ import { environment } from 'src/environments/environment';
 export class AppComponent {
   title = 'lil-big-things-3d';
   app = initializeApp(environment.firebase);
-  auth = getAuth();
-
-  signUp(email: string, password: string) {
-    createUserWithEmailAndPassword(this.auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-  }
 }
--- lil-big-things-3d/src/app/services/authentication.service.ts
+++ lil-big-things-3d/src/app/services/authentication.service.ts
@@ -1,8 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@angular/core';
+import { createUserWithEmailAndPassword, getAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  auth = getAuth();
 
+  signUpWithEmail(email: string, password: string) {
+    createUserWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
 }
