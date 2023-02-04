a11333e3279f8b118709f49a4c7034fe298ca09c
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-02-04 14:49:59 +0200

    get userID added to auth service

--- lil-big-things-3d/src/app/services/authentication.service.ts
+++ lil-big-things-3d/src/app/services/authentication.service.ts
@@ -1,24 +1,41 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@angular/core';
-import { createUserWithEmailAndPassword, getAuth } from '@angular/fire/auth';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  auth = getAuth();
+    auth = getAuth();
 
-  signUpWithEmail(email: string, password: string) {
-    createUserWithEmailAndPassword(this.auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
+    get userID() {
+        onAuthStateChanged(this.auth, (user) => {
+            if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.User
+            return user.uid;
+            } 
+            return null
+        });
+        // User is signed out
         // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // ..
-      });
-  }
+        return null
+    } 
+
+    signUpWithEmail(email: string, password: string) {
+        createUserWithEmailAndPassword(this.auth, email, password)
+            .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            // ...
+            })
+            .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+            });
+    }
+
+
+
 }
