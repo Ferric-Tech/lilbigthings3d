d50da0074bcda585847d50f4b41c5fbae4c69331
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-02-05 11:36:41 +0200

    Routing modification - Adding dashboard and login as childern to admin page

--- lil-big-things-3d/src/app/app-routing.module.ts
+++ lil-big-things-3d/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './pages/admin-page/admin-dashboard/admin-dashboard.component';
+import { AdminLoginComponent } from './pages/admin-page/admin-login/admin-login.component';
 import { AdminPageComponent } from './pages/admin-page/admin.page';
 import { HomePageComponent } from './pages/home-page/home.page';
 
@@ -8,16 +10,16 @@ const routes: Routes = [
   {
     path: 'admin',
     component: AdminPageComponent,
-    // children: [
-    //   {
-    //     path: 'child-a', // child route path
-    //     component: ChildAComponent, // child route component that the router renders
-    //   },
-    //   {
-    //     path: 'child-b',
-    //     component: ChildBComponent, // another child route component that the router renders
-    //   },
-    // ],
+    children: [
+      {
+        path: 'login',
+        component: AdminLoginComponent,
+      },
+      {
+        path: 'dashboard',
+        component: AdminDashboardComponent,
+      },
+    ],
   },
 ];
 
