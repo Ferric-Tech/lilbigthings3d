dc4a30da354de3a09c583d6b2f4ab8204aedb40f
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-02-05 12:24:34 +0200

    ErrorModal implimented in App

--- lil-big-things-3d/src/app/modals/error-modal/error-modal.component.ts
+++ lil-big-things-3d/src/app/modals/error-modal/error-modal.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ErrorHandlingService } from 'src/app/services/error-handling.service';
 
 @Component({
   selector: 'app-error-modal',
   templateUrl: './error-modal.component.html',
-  styleUrls: ['./error-modal.component.scss']
+  styleUrls: ['./error-modal.component.scss'],
 })
-export class ErrorModalComponent {
+export class ErrorModalComponent implements OnInit {
+  constructor(private readonly errorService: ErrorHandlingService) {}
 
+  ngOnInit() {
+    this.errorService.errorModalContent.subscribe((content) => {
+      console.log(content);
+    });
+  }
 }
--- lil-big-things-3d/src/app/services/error-handling.service.ts
+++ lil-big-things-3d/src/app/services/error-handling.service.ts
@@ -1,10 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface ErrorModalContent {
+  title: string;
+  code: string;
+  message: string;
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ErrorHandlingService {
+  errorModelObs$: BehaviorSubject<ErrorModalContent> = new BehaviorSubject(
+    {} as ErrorModalContent
+  );
+  errorModalContent = this.errorModelObs$.asObservable();
+
   handError(error: unknown, message?: unknown): void {
     message ? console.log(error) : console.log(error);
+    this.errorModelObs$.next({
+      title: 'Something went wrong',
+      code: error as string,
+      message: message as string,
+    });
   }
 }
