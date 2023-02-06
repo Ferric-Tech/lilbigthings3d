086489fa88447eed52b9578bb712f09609d146ac
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-02-06 17:59:29 +0200

    Improvements made to add-product component to make form more dynamic

--- lil-big-things-3d/src/app/pages/admin-page/admin-dashboard/add-product/add-product.component.ts
+++ lil-big-things-3d/src/app/pages/admin-page/admin-dashboard/add-product/add-product.component.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { FirestoreManagementService } from 'src/app/services/firestore-management/firestore-management.service';
 
 export interface Product {
@@ -10,23 +16,118 @@ export interface Product {
   imageFiles?: File[];
 }
 
+export interface FormFieldConfig {
+  name: string;
+  type: FormFieldType;
+  label?: string;
+  placeholder?: string;
+  validators?: ValidatorFn[];
+}
+
+export enum FormFieldType {
+  MainLabel,
+  LabelSub,
+  UnderlinedLabel,
+  PlainLabel,
+  InputTitle,
+  InputPlain,
+  InputLong,
+  UploaderSingleFileUnderlined,
+  UploaderSingleFilePlain,
+  UploaderMultiFileUnderlined,
+  UploaderMultiFilePlain,
+}
+
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.scss'],
 })
-export class AddProductComponent {
+export class AddProductComponent implements OnInit {
   printFiles: File[] = [];
   imageFiles: File[] = [];
-  basicDetailsForm = this.fb.group({
-    title: ['New Product', Validators.required],
-    decription: ['Provide a description', Validators.required],
-  });
+
+  basicDetailsForm = this.fb.group({});
+  filesForm = this.fb.group({});
+
+  formFieldType = FormFieldType;
+  basicDetailsFormConfig: FormFieldConfig[] = [
+    {
+      name: 'title',
+      type: FormFieldType.InputTitle,
+      placeholder: 'New Product',
+      validators: [Validators.required],
+    },
+    {
+      name: 'description',
+      type: FormFieldType.InputLong,
+      placeholder: 'Please provide a description',
+      validators: [Validators.required],
+    },
+  ];
+
+  filesFormConfig: FormFieldConfig[] = [
+    {
+      name: 'files-lable',
+      type: FormFieldType.LabelSub,
+      label: 'Files',
+    },
+    {
+      name: 'design-files-uploader',
+      type: FormFieldType.UploaderSingleFileUnderlined,
+      placeholder: 'None',
+      label: 'Design file',
+    },
+    {
+      name: 'print-files-label',
+      type: FormFieldType.UnderlinedLabel,
+      placeholder: 'None',
+      label: 'Print file',
+    },
+    {
+      name: 'print-files-fast',
+      type: FormFieldType.UploaderSingleFilePlain,
+      placeholder: 'None',
+      label: 'Fast',
+    },
+    {
+      name: 'print-files-standard',
+      type: FormFieldType.UploaderSingleFilePlain,
+      placeholder: 'None',
+      label: 'Standard',
+    },
+    {
+      name: 'print-files-optimised',
+      type: FormFieldType.UploaderSingleFilePlain,
+      placeholder: 'None',
+      label: 'Optimised',
+    },
+    {
+      name: 'design-files-custom',
+      type: FormFieldType.UploaderSingleFilePlain,
+      placeholder: 'None',
+      label: 'Custom',
+    },
+  ];
 
   constructor(
     readonly fs: FirestoreManagementService,
     private fb: FormBuilder
   ) {}
+
+  ngOnInit(): void {
+    this.setForm(this.basicDetailsForm, this.basicDetailsFormConfig);
+    this.setForm(this.filesForm, this.filesFormConfig);
+  }
+
+  private setForm(form: FormGroup, config: FormFieldConfig[]): void {
+    config.forEach((field: FormFieldConfig) => {
+      form.addControl(
+        field.name,
+        new FormControl(field.placeholder || '', field.validators)
+      );
+    });
+  }
 
   onPrintFileSelection(event: any): void {
     for (const file of event.target.files) {
@@ -41,15 +142,15 @@ export class AddProductComponent {
   }
 
   onSubmit() {
-    if (!this.basicDetailsForm.valid || !this.printFiles) {
-      return;
-    }
-    const product: Product = {
-      title: this.basicDetailsForm.value.title || '',
-      decription: this.basicDetailsForm.value.decription || '',
-      printFiles: this.printFiles,
-      imageFiles: this.imageFiles,
-    };
-    this.fs.addProduct(product);
+    // if (!this.basicDetailsForm.valid || !this.printFiles) {
+    //   return;
+    // }
+    // const product: Product = {
+    //   title: this.basicDetailsForm.value.title || '',
+    //   decription: this.basicDetailsForm.value.description || '',
+    //   printFiles: this.printFiles,
+    //   imageFiles: this.imageFiles,
+    // };
+    // this.fs.addProduct(product);
   }
 }
