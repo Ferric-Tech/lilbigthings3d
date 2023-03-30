fa8837ec7033301a2de7f04498b925f0e386a1ea
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-03-30 09:26:04 +0200

    Firebase function initalised

--- lil-big-things-3d/functions/src/index.ts
+++ lil-big-things-3d/functions/src/index.ts
@@ -1,4 +1,5 @@
-import * as functions from "firebase-functions";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +8,16 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// How to write functions accross multiple files:
+// https://firebase.google.com/docs/functions/organize-functions#write_functions_in_multiple_files
+
+const admin = require('firebase-admin');
+const cors = require('cors')({ origin: true });
+const functions = require('firebase-functions');
+admin.initializeApp(functions.config().firebase);
+
+const paymentNotificationsModule = require('./modules/payment-notifications');
+
+exports.processPaymentNotification =
+  paymentNotificationsModule.processPaymentNotification;
