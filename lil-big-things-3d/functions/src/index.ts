c74d905b406b9aab77abbb5d0f522077ef49b86d
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2023-03-30 12:44:42 +0200

    Refactor of ProcessPaymentNotification function

--- lil-big-things-3d/functions/src/index.ts
+++ lil-big-things-3d/functions/src/index.ts
@@ -11,95 +11,6 @@ import axios from 'axios';
 
 exports.processPaymentNotification = functions.https.onRequest(
   async (req, res) => {
-    // Grab the text parameter.
-    const notification = req.body;
-    let pfParamString = '';
-    const passPhrase = 'SaltAndPepperPig'; // Testing
-    const cartTotal = notification['amount_gross'];
-    const testingMode = true;
-    const pfHost = testingMode ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
-
-    for (const key in notification) {
-      if (key in notification && key !== 'signature') {
-        pfParamString += `${key}=${encodeURIComponent(
-          notification[key].trim()
-        ).replace(/%20/g, '+')}&`;
-      }
-    }
-
-    // Remove last ampersand
-    pfParamString = pfParamString.slice(0, -1);
-
-    const pfValidSignature = (
-      notification: Record<string, unknown>,
-      pfParamString: string,
-      pfPassphrase: string | null = null
-    ) => {
-      // Calculate security signature
-      const tempParamString = '';
-      if (pfPassphrase !== null) {
-        pfParamString += `&passphrase=${encodeURIComponent(
-          pfPassphrase.trim()
-        ).replace(/%20/g, '+')}`;
-      }
-
-      const signature = crypto
-        .createHash('md5')
-        .update(pfParamString)
-        .digest('hex');
-      return notification['signature'] === signature;
-    };
-
-    async function ipLookup(domain: string): Promise<unknown[]> {
-      return new Promise((resolve, reject) => {
-        dns.lookup(
-          domain,
-          { all: true },
-          (err: unknown, address: { address: unknown }[]) => {
-            if (err) {
-              reject(err);
-            } else {
-              const addressIps = address.map(function (item: {
-                address: unknown;
-              }) {
-                return item.address;
-              });
-              resolve(addressIps);
-            }
-          }
-        );
-      });
-    }
-
-    const pfValidIP = async (req: functions.https.Request) => {
-      const validHosts = [
-        'www.payfast.co.za',
-        'sandbox.payfast.co.za',
-        'w1w.payfast.co.za',
-        'w2w.payfast.co.za',
-      ];
-
-      let validIps: Iterable<unknown> | null | undefined = [];
-      const pfIp =
-        req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-
-      try {
-        for (const key in validHosts) {
-          const ips = await ipLookup(validHosts[key]);
-          validIps = [...validIps, ...ips];
-        }
-      } catch (err) {
-        console.error(err);
-      }
-
-      const uniqueIps = [...new Set(validIps)];
-
-      if (uniqueIps.includes(pfIp)) {
-        return true;
-      }
-      return false;
-    };
-
     const pfValidPaymentData = (
       cartTotal: string,
       pfData: { [x: string]: string }
@@ -125,21 +36,117 @@ exports.processPaymentNotification = functions.https.onRequest(
       return result === 'VALID';
     };
 
-    const check1 = pfValidSignature(notification, pfParamString, passPhrase);
-    const check2 = pfValidIP(req);
-    const check3 = pfValidPaymentData(cartTotal, notification);
-    const check4 = pfValidServerConfirmation(pfHost, pfParamString);
+    const testingMode = true;
+    const passPhrase = testingMode ? 'SaltAndPepperPig' : 'ThisIsThe1AndOnly'; // Testing
+    const pfHost = testingMode ? 'sandbox.payfast.co.za' : 'www.payfast.co.za';
 
-    if (check1 && (await check2) && check3 && (await check4)) {
+    const paymentNotificationContent = req.body;
+
+    const cartTotal = paymentNotificationContent['amount_gross'];
+    const paramString = generateParamString(paymentNotificationContent);
+
+    const isValidSignature = isValidSignatureCheck(
+      paymentNotificationContent,
+      paramString,
+      passPhrase
+    );
+    const isValidIP = await isValidIPCheck(req);
+    const check3 = pfValidPaymentData(cartTotal, paymentNotificationContent);
+    const check4 = pfValidServerConfirmation(pfHost, paramString);
+
+    if (isValidSignature && isValidIP && check3 && (await check4)) {
       // Push the new message into Firestore using the Firebase Admin SDK.
-      const writeResult = await admin
-        .firestore()
-        .collection('payment-notifications')
-        .add(notification);
+      paymentNotificationContent['matched'] = true;
       // Send back a message that we've successfully written the message
-      res.json({ result: `Message with ID: ${writeResult.id} added.` });
     } else {
-      // Some checks have failed, check payment manually and log for investigation
+      paymentNotificationContent['matched'] = false;
     }
+
+    const writeResult = await admin
+      .firestore()
+      .collection('payment-notifications')
+      .add(paymentNotificationContent);
+    res.json({ result: `Message with ID: ${writeResult.id} added.` });
   }
 );
+
+function generateParamString(
+  paymentNotificationContent: Record<string, string>
+): string {
+  let returnString = '';
+  for (const key in paymentNotificationContent) {
+    if (key in paymentNotificationContent && key !== 'signature') {
+      returnString += `${key}=${encodeURIComponent(
+        paymentNotificationContent[key].trim()
+      ).replace(/%20/g, '+')}&`;
+    }
+  }
+  // Remove last ampersand
+  returnString = returnString.slice(0, -1);
+  return returnString;
+}
+
+function isValidSignatureCheck(
+  paymentNotificationContent: Record<string, string>,
+  paramString: string,
+  passphrase: string | null = null
+): boolean {
+  // Calculate security signature
+  //   const tempParamString = '';
+  if (passphrase !== null) {
+    paramString += `&passphrase=${encodeURIComponent(passphrase.trim()).replace(
+      /%20/g,
+      '+'
+    )}`;
+  }
+
+  const signature = crypto.createHash('md5').update(paramString).digest('hex');
+  return paymentNotificationContent['signature'] === signature;
+}
+
+async function ipLookup(domain: string): Promise<unknown[]> {
+  return new Promise((resolve, reject) => {
+    dns.lookup(
+      domain,
+      { all: true },
+      (err: unknown, address: { address: unknown }[]) => {
+        if (err) {
+          reject(err);
+        } else {
+          const addressIps = address.map(function (item: { address: unknown }) {
+            return item.address;
+          });
+          resolve(addressIps);
+        }
+      }
+    );
+  });
+}
+
+async function isValidIPCheck(req: functions.https.Request): Promise<boolean> {
+  const validHosts = [
+    'www.payfast.co.za',
+    'sandbox.payfast.co.za',
+    'w1w.payfast.co.za',
+    'w2w.payfast.co.za',
+  ];
+
+  let validIps: Iterable<unknown> | null | undefined = [];
+  const pfIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  try {
+    for (const key in validHosts) {
+      const ips = await ipLookup(validHosts[key]);
+      validIps = [...validIps, ...ips];
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  const uniqueIps = [...new Set(validIps)];
+
+  if (uniqueIps.includes(pfIp)) {
+    return true;
+  }
+  return false;
+}
